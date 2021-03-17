df62f49d516245fef119da0870b0e197beb155e6
Olivier Baillard <olivier.baillard@web.de>
2021-03-17 21:52:14 +0100

    Adding heroku required files

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -14,12 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'olivier'  # for JWT: very important to set (should be encrypted not like here)
 api = Api(app)
-
-
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 
 jwt = JWT(app, authenticate, identity)  # JWT create a new endpoint '/auth'
 
